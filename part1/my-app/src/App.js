be2306ba27e29e74923f9d65f5d5b77b3b1d3d09
Sonal2808 <135756443+Sonal2808@users.noreply.github.com>
2023-07-15 15:53:00 -0500

    check

---
+++ part1/my-app/src/App.js
@@ -0,0 +1,15 @@
+//import logo from './logo.svg';
+import './App.css';
+import Description from './components/Description.js';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Description/>    
+       
+    </div>
+  );
+}
+
+export default App;
---
+++ part1/my-app/src/components/Description.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import styles from './Description.module.css';
+function RecipeAuthor(){
+    let authorLink = "https://pinchofyum.com/";
+    let authorPhoto = "https://pinchofyum.com/wp-content/assets/images/home-lindsay-kitchen-380x480.jpg";
+    let authorName = "LINDSAY";
+    return (
+        <div className = {styles.recipeAuthorBlock}>
+            
+           <img src={authorPhoto} alt = "Lindsay-Kitchen" className={styles.imageUpdates} />
+           <div className = {styles.recipeDescriptionBlock}>
+              <h3>{authorName}</h3>
+              <a href={authorLink}>Blog name</a> 
+           </div>
+        </div>
+     );
+}
+class RecipeDescription extends React.Component {
+    render() {
+        return (
+<div> 
+<div>
+   <h1>Recipe Title</h1>
+   <p>Short recipe description</p>
+</div>
+<RecipeAuthor />
+</div>
+        );
+    };
+};
+
+export default RecipeDescription;
