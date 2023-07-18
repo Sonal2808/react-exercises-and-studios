1452f3c911052aed3108085fe500d2d4959cbf36
Sonal2808 <135756443+Sonal2808@users.noreply.github.com>
2023-07-18 13:45:15 -0500

    done exercise

--- part2/exercises-2/src/components/Profile.js
+++ part2/exercises-2/src/components/Profile.js
@@ -1,12 +1,27 @@
 import './styles.css';
 import Button from './Button';
-
-
+import oceans from './oceans.json';
+let listItem = oceans.map(oceans =>
+   <div key={oceans.id}  className={`${oceans.fishCheck === "true" ? "isAFish" : "profile"}`}>
+   <img src={oceans.image} alt={oceans.name} className="img"/>
+   <h1>{oceans.name}</h1>
+   <h3>Fun Facts:</h3>
+      <ul>
+      <li>{oceans.fact1}</li>
+      <li>{oceans.fact2}</li>
+      <li>{oceans.fact3}</li>
+      </ul> 
+   </div>
+);
 function Profile()
 {
    return(
+      
       <>
-         <h3>Coming Soon! Profiles of Ocean creatures</h3>
+
+        
+       {listItem}
+          
          <Button />
       </>  
    );
